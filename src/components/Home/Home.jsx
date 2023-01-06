b33feb7136a9fc97f9f343d133c887b01716f8b8
Muhammad Saad <muhammadsaad2522@gmail.com>
2023-01-06 15:59:10 +0500

    home page ui ready

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
+import baryani from '../Assets/baryani.jpg'
+import burger from '../Assets/burger.jpg'
+import pizza from '../Assets/pizza.jpg'
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
             <Navbar />
-            <Link to={"/baryani"}>
-                baryani
-            </Link>
-            <Link to={"/burger"}>
-                baryani
-            </Link>
-            <Link to={"/pizza"}>
-                baryani
-            </Link>
+            <div className='hContainer'>
+                <div className='br box'>
+                    <h1>Biryani</h1>
+                    <Link to={"/baryani"}>
+                        <img src={baryani} alt="" />
+                    </Link>
+                </div>
+                <div className='bg box'>
+                <h1>Burger</h1>
+                    <Link to={"/burger"}>
+                        <img src={burger} alt="" />
+                    </Link>
+                </div>
+                <div className='pz box'>
+                <h1>Pizza</h1>
+                    <Link to={"/pizza"}>
+                        <img src={pizza} alt="" />
+                    </Link>
+                </div>
+            </div>
+
         </div>
     )
 }
