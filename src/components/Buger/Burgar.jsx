05ad795a7caab74c803d4359f002e97c0a4e65c5
Muhammad Saad <muhammadsaad2522@gmail.com>
2023-01-06 15:19:36 +0500

    create componenets

---
+++ src/components/Buger/Burgar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+const Burgar = () => {
+    return (
+        <div>
+            <Navbar />
+            Burgar
+        </div>
+    )
+}
+
+export default Burgar
